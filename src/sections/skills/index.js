4f9153146ced5db4cc63065899fc11cda5d3228b
josedrojasa <josedrojasa@hotmail.com>
2024-05-12 15:51:32 -0500

    Fix responsive

--- src/sections/skills/index.js
+++ src/sections/skills/index.js
@@ -12,10 +12,10 @@ export const Skills = () => {
         }}
       >
         <div className="relative">
-          <img src={Cross} alt="Cross" className="absolute inset-0 m-auto" />
-          <div className="grid grid-cols-2 gap-14 max-w-screen-lg">
-            <div className="flex flex-col">
-              <h2 className="text-4xl font-semibold">UX/UI</h2>
+          <img src={Cross} alt="Cross" className="absolute inset-0 m-auto md:rotate-0 rotate-45 ease-linear" />
+          <div className="flex flex-wrap max-w-screen-lg p-4">
+            <div className="flex flex-col md:w-2/4 w-full items-center md:items-start mt-[-100px] md:mt-0 md:mb-20">
+              <h2 className="text-2xl md:text-4xl font-semibold mb-2 md:mb-8">UX/UI</h2>
               <label>Figma</label>
               <label>Wireframing and prototyping</label>
               <label>UX research</label>
@@ -25,22 +25,21 @@ export const Skills = () => {
               <label>CSS</label>
               <label>HTML</label>
             </div>
-            <div className="flex flex-col items-end">
-              <h2 className="text-4xl font-semibold">DATA</h2>
+            <div className="flex flex-col items-start md:items-end w-2/4">
+              <h2 className="text-2xl md:text-4xl font-semibold mb-2 md:mb-8">DATA</h2>
               <label>Microsoft Power BI</label>
               <label>Microsoft Excel</label>
               <label>Python</label>
             </div>
-            <div className="flex flex-col">
-              <h2 className="text-4xl font-semibold">PROJECTS</h2>
+            <div className="flex flex-col w-2/4 items-end md:items-start">
+              <h2 className="text-2xl md:text-4xl font-semibold mb-2 md:mb-8">PROJECTS</h2>
               <label>Scrum</label>
               <label>Jira</label>
               <label>Confluence</label>
               <label>Culture</label>
-              <div className="h-16"></div>
             </div>
-            <div className="flex flex-col items-end">
-              <h2 className="text-4xl font-semibold">BUSINESS</h2>
+            <div className="flex flex-col md:w-2/4 w-full items-center md:items-end mt-12 md:mt-0">
+              <h2 className="text-2xl md:text-4xl font-semibold mb-2 md:mb-8">BUSINESS</h2>
               <label>Market Research</label>
               <label>Email marketing/ Hubspot</label>
               <label>Digital Marketing</label>
