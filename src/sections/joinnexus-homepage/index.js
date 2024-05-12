f2310d74ab805766ef871bc4a0f29305df7295df
Jose David Rojas Aguilar <josedrojasa@hotmail.com>
2024-05-12 03:32:27 +0000

    Add joinnexus platform

--- src/sections/joinnexus-homepage/index.js
+++ src/sections/joinnexus-homepage/index.js
@@ -17,46 +17,46 @@ import Image43 from './assets/image4-3.png';
 import Styles from './index.module.css';
 
 export const JoinnexusHomepage = () => {
-    return (<SectionContainer>
-        <GradientContainer color="#5215D3">
-            <div className="flex flex-col gap-16">
-                <h1 className="text-center text-white text-6xl font-light">Joinnexus Homepage</h1>
-                <div className="max-w-screen-lg">
-                    <GradientCard
-                        fromColor="#11021F"
-                        midColor="#160329"
-                        toColor="#150524"
-                    >
-                        <div className={`grid gap-16 ${Styles.imagesContainer}`}>
-                            <div className="flex flex-col">
-                                <img alt="joinnexus screenshot" src={Image11} />
-                                <img alt="joinnexus screenshot" src={Image12} />
-                                <img alt="joinnexus screenshot" src={Image13} />
+    return (
+        <SectionContainer>
+            <GradientContainer color="#5215D3">
+                <div className="flex flex-col gap-16">
+                    <h1 className="text-center text-white text-6xl font-light">Joinnexus Homepage</h1>
+                    <div className="max-w-screen-lg">
+                        <GradientCard
+                            fromColor="#11021F"
+                            midColor="#160329"
+                            toColor="#150524"
+                        >
+                            <div className={`grid gap-16 ${Styles.imagesContainer}`}>
+                                <div className="flex flex-col">
+                                    <img alt="joinnexus screenshot" src={Image11} />
+                                    <img alt="joinnexus screenshot" src={Image12} />
+                                    <img alt="joinnexus screenshot" src={Image13} />
 
+                                </div>
+                                <div className="flex flex-col">
+                                    <img alt="joinnexus screenshot" src={Image21} />
+                                    <img alt="joinnexus screenshot" src={Image22} />
+                                    <img alt="joinnexus screenshot" src={Image23} />
+                                </div>
+                                <div className="hidden md:flex flex-col">
+                                    <img className={`max-w-24 ${Styles.horizontalImage}`} alt="joinnexus screenshot" src={Image31} />
+                                    <img className={`max-w-24 ${Styles.horizontalImage}`} alt="joinnexus screenshot" src={Image32} />
+                                    <img className={`max-w-24 ${Styles.horizontalImage}`} alt="joinnexus screenshot" src={Image33} />
+                                </div>
+                                <div className="hidden md:flex flex-col">
+                                    <img className={`max-w-24 ${Styles.horizontalImage}`} alt="joinnexus screenshot" src={Image41} />
+                                    <img className={`max-w-24 ${Styles.horizontalImage}`} alt="joinnexus screenshot" src={Image42} />
+                                    <img className={`max-w-24 ${Styles.horizontalImage}`} alt="joinnexus screenshot" src={Image43} />
+                                </div>
                             </div>
-                            <div className="flex flex-col">
-                                <img alt="joinnexus screenshot" src={Image21} />
-                                <img alt="joinnexus screenshot" src={Image22} />
-                                <img alt="joinnexus screenshot" src={Image23} />
-                            </div>
-                            <div className="hidden md:flex flex-col">
-                                <img className={`max-w-24 ${Styles.horizontalImage}`} alt="joinnexus screenshot" src={Image31} />
-                                <img className={`max-w-24 ${Styles.horizontalImage}`} alt="joinnexus screenshot" src={Image32} />
-                                <img className={`max-w-24 ${Styles.horizontalImage}`} alt="joinnexus screenshot" src={Image33} />
-                            </div>
-                            <div className="hidden md:flex flex-col">
-                                <img className={`max-w-24 ${Styles.horizontalImage}`} alt="joinnexus screenshot" src={Image41} />
-                                <img className={`max-w-24 ${Styles.horizontalImage}`} alt="joinnexus screenshot" src={Image42} />
-                                <img className={`max-w-24 ${Styles.horizontalImage}`} alt="joinnexus screenshot" src={Image43} />
-                            </div>
-                        </div>
-                    </GradientCard>
+                        </GradientCard>
+                    </div>
+                    <div className="flex items-center justify-center">
+                        <Button href="https://joinnexus.io">Visit here</Button>
+                    </div>
                 </div>
-                <div className="flex items-center justify-center">
-                    <Button href="https://joinnexus.io">Visit here</Button>
-                </div>
-
-            </div>
-        </GradientContainer>
-    </SectionContainer>);
+            </GradientContainer>
+        </SectionContainer>);
 }
--- src/sections/joinnexus-platform/index.js
+++ src/sections/joinnexus-platform/index.js
@@ -1,5 +1,41 @@
+import { Button } from "../../components/button";
+import { GradientCard } from "../../components/gradient-card";
+import { GradientContainer } from "../../components/gradient-container";
 import { SectionContainer } from "../../hoc/section-container";
+import Image1 from './assets/image1.png';
+import Image2 from './assets/image2.png';
+import Image3 from './assets/image3.png';
+import Image4 from './assets/image4.png';
+import Image5 from './assets/image5.png';
+import Image6 from './assets/image6.png';
 
 export const JoinnexusPlatform = () => {
-    return <SectionContainer></SectionContainer>;
+    return (
+        <SectionContainer>
+            <GradientContainer color="#5215D3">
+                <div className="flex flex-col gap-16">
+                    <h1 className="text-center text-white text-6xl font-light">Joinnexus Platform</h1>
+                    <div className="max-w-screen-lg">
+                        <GradientCard
+                            fromColor="#501986B2"
+                            midColor="#471677B2"
+                            toColor="#2F0D50B2"
+                        >
+                            <div className="grid grid-cols-3 gap-4">
+                                <img className="h-full" src={Image1} alt="Joinnexus screenshot" />
+                                <img className="h-full" src={Image2} alt="Joinnexus screenshot" />
+                                <img className="h-full" src={Image3} alt="Joinnexus screenshot" />
+                                <img className="h-full" src={Image4} alt="Joinnexus screenshot" />
+                                <img className="h-full" src={Image5} alt="Joinnexus screenshot" />
+                                <img className="h-full" src={Image6} alt="Joinnexus screenshot" />
+                            </div>
+                        </GradientCard>
+                    </div>
+                    <div className="flex items-center justify-center">
+                        <Button href="https://joinnexus.io">Visit here</Button>
+                    </div>
+                </div>
+            </GradientContainer>
+        </SectionContainer>
+    );
 }
