08e356aab9a9f24ebee86d3761b4265b1be35009
Jose David Rojas Aguilar <josedrojasa@hotmail.com>
2024-05-12 02:50:40 +0000

    Fix responsive

--- src/sections/title/index.js
+++ src/sections/title/index.js
@@ -6,12 +6,12 @@ export const Title = () => {
     <SectionContainer className="grid">
       <div className="h-full w-full bg-[#201b1b] flex items-center justify-center">
         <div className="flex flex-col gap-8">
-          <div className="flex text-white text-9xl font-bold">
+          <div className="flex text-white text-7xl md:text-9xl font-bold">
             <h1 className={Styles.yLetter}>Y</h1>
             <h1>ALETOR</h1>
           </div>
 
-          <h2 className="text-white text-7xl text-center font-medium">
+          <h2 className="text-white text-5xl md:text-7xl text-center font-medium">
             I do UX/UI
           </h2>
         </div>
