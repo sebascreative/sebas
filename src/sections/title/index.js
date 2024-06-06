3e303e3eb1ff41ff04d252ed50ead1a0296c62c5
sebascreative <pruebasdesarrollopython@outlook.com>
2024-06-06 23:06:33 +0000

    Change title background, fonts

--- src/sections/title/index.js
+++ src/sections/title/index.js
@@ -2,15 +2,15 @@ import Styles from "./index.module.css";
 import { SectionContainer } from "../../hoc/section-container";
 import Profile from "./assets/profile.png";
 import { ReactTyped } from "react-typed";
-
+import background from "./assets/title.jpg";
 export const Title = () => {
   return (
     <SectionContainer className="grid">
-      <div className="h-full w-full bg-[#201b1b] flex items-center justify-center">
+      <div style= {{background:`url("${background}")`}}className="h-full w-full  flex items-center justify-center">
         <div className="flex flex-col gap-8">
           <div className="flex text-white text-7xl md:text-9xl font-bold items-center justify-center">
-            <h1 className={Styles.yLetter}>Y</h1>
-            <h1>ALETOR</h1>
+        
+            <h1>Hi there!</h1>
           </div>
           <div className="md:hidden flex items-center justify-center p-8">
             <img
