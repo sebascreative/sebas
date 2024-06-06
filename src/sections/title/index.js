01331e0e7cfc3dea2a66632bb33e84d0c76b4e04
sebascreative <pruebasdesarrollopython@outlook.com>
2024-06-06 23:23:56 +0000

    Add logo, and "sebas.." text

--- src/sections/title/index.js
+++ src/sections/title/index.js
@@ -1,12 +1,20 @@
-import Styles from "./index.module.css";
 import { SectionContainer } from "../../hoc/section-container";
 import Profile from "./assets/profile.png";
 import { ReactTyped } from "react-typed";
 import background from "./assets/title.jpg";
+import logo from "./assets/logo.png"
 export const Title = () => {
   return (
     <SectionContainer className="grid">
-      <div style= {{background:`url("${background}")`}}className="h-full w-full  flex items-center justify-center">
+      <div style= {{background:`url("${background}")`}}className="h-full w-full flex items-center justify-center">
+        <div className="absolute top-4 left-9">
+          <div className="flex gap-2">
+            <img src={logo} className="w-32 h-32" alt="logo"></img>
+            <div className="flex items-center justify-center">
+              <label className="text-white text-2xl">sebascreative</label>
+            </div>
+          </div>
+        </div>
         <div className="flex flex-col gap-8">
           <div className="flex text-white text-7xl md:text-9xl font-bold items-center justify-center">
         
